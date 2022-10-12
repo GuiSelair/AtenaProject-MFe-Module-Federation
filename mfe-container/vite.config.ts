5169ca6a539cbdf1c1d3ae1a72dd0c5a72abd41e
Guilherme Selair <guilhermeselair@gmail.com>
2022-10-12 11:15:23 -0300

    Feat: Added ViteModuleFederation

--- mfe-container/vite.config.ts
+++ mfe-container/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import federation from "@originjs/vite-plugin-federation"
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    federation({
+      name: 'CONTAINER',
+      filename: 'remoteEntry.js',
+      remotes:{
+          some: 'remote_some'
+      },
+      shared: ['react']
+    })
+  ]
 })
